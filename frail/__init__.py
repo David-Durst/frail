17fce927f08892c9d8d87e4d7b753437ec8b9133
David Durst <davidbdurst@gmail.com>
2020-12-29 13:44:00 -0800

    added print examples

--- frail/__init__.py
+++ frail/__init__.py
@@ -3,3 +3,4 @@ from .examples import *
 from .debug_printer import *
 from .smt_printer import *
 from .smt_utils import *
+from .verilog_printer import print_verilog
---
+++ run/verilog_print_examples.sh
@@ -0,0 +1,2 @@
+#python -c 'import frail; print("design b:"); frail.print_verilog(frail.design_b); print("design a:"); frail.print_verilog(frail.design_a)'
+python -c 'import frail; print("design b:"); frail.print_verilog(frail.design_b);'
