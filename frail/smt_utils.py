93a1f68586f8ca0720ae4d6267085ae50d3987cd
David Durst <davidbdurst@gmail.com>
2020-12-31 09:25:33 -0800

    adding BVSubExtend

--- frail/smt_utils.py
+++ frail/smt_utils.py
@@ -1,4 +1,4 @@
-from pysmt.shortcuts import BVZExt, BVAdd, BVMul, Equals
+from pysmt.shortcuts import BVZExt, BVAdd, BVSub, BVMul, Equals
 
 def BVAddExtend(t0,t1):
   orig_width_t0 = t0.get_type().width
@@ -7,6 +7,14 @@ def BVAddExtend(t0,t1):
   t0e = BVZExt(t0, max(orig_width-orig_width_t0, 0))
   t1e = BVZExt(t1, max(orig_width-orig_width_t1, 0))
   return BVAdd(t0e, t1e)
+
+def BVSubExtend(t0,t1):
+  orig_width_t0 = t0.get_type().width
+  orig_width_t1 = t1.get_type().width
+  orig_width = max(orig_width_t0,orig_width_t1)
+  t0e = BVZExt(t0, max(orig_width-orig_width_t0, 0))
+  t1e = BVZExt(t1, max(orig_width-orig_width_t1, 0))
+  return BVSub(t0e, t1e)
 
 def BVMulExtend(t0,t1):
   orig_width_t0 = t0.get_type().width
