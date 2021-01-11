501e0259bc3096b232b93a52c1894c57b7cd4d13
Kavya Sreedhar <skavya@stanford.edu>
2021-01-10 18:05:36 -0800

    tb for opdesign6

--- tests/test_designab.py
+++ tests/test_designab.py
@@ -33,22 +33,9 @@ def test_addr_design(
 
     print(starting_addr, strides_0, strides_1, ranges_0, ranges_1)
 
-    # set up addressor model
-    model_ag = AddrGenModel(2, 16)
-
-    config = {}
-    config["starting_addr"] = starting_addr
-    config["dimensionality"] = 2
-    config["strides_0"] = strides_0
-    config["strides_1"] = strides_1
-    config["ranges_0"] = ranges_0
-    config["ranges_1"] = ranges_1
-
-    model_ag.set_config(config)
-
     # set up frail design with Verilog
     frail_dir = pathlib.Path(__file__).parent.parent.absolute()
-
+    print(frail_dir)
     # get Magma circuit from Verilog
     dut = m.define_from_verilog_file(
         f"{frail_dir}/verilog/{design}.v",
@@ -62,45 +49,46 @@ def test_addr_design(
 
     # no need to rst_n or clk_en yet
 
-    # config regs
-    if design == "design_b" or design == "op_design":
-        tranges, tstrides = transform_strides_and_ranges(
-            [ranges_0, ranges_1],
-            [strides_0, strides_1],
-            2)
-        tester.circuit.x_max = ranges_0 #tranges[0]
-        tester.circuit.x_stride = tstrides[0]
-        tester.circuit.y_max = ranges_1 #tranges[1]
-        tester.circuit.y_stride = tstrides[1]
-        tester.circuit.offset = starting_addr
-        print("transformed:", tranges, tstrides)
-    else:
-        tester.circuit.x_max = ranges_0
-        tester.circuit.x_stride = strides_0
-        tester.circuit.y_max = ranges_1
-        tester.circuit.y_stride = strides_1
-        tester.circuit.offset = starting_addr
+    x = [f"ranges_{i}" for i in range(4)]
+    y = [f"strides_{i}" for i in range(4)]
+    r = [2, 4, 14, 1]
+    s = [4, 1, 4, 0]
+    tester.circuit.dim = 4
+    for i in range(len(x)):
+        setattr(tester.circuit, x[i], r[i])
+        setattr(tester.circuit, y[i], s[i])
+
+    # set up addressor model
+    model_ag = AddrGenModel(4, 16)
+
+    config = {}
+    for i in range(len(x)):
+        config[x[i]] = r[i]
+        config[y[i]] = s[i]
+    config["dimensionality"] = 4
+
+    model_ag.set_config(config)
 
     tester.circuit.step = 1
 
-    for i in range(min(1000, ranges_0 * ranges_1 - 1)):
+    for i in range(min(1000, 2*4*14 - 1)):
         # start with first addr on rising clk edge
         tester.circuit.clk = 1
         tester.step(2)
         tester.eval()
         model_ag.step()
-        tester.circuit.addr.expect(model_ag.get_address())
+        tester.circuit.addr_out.expect(model_ag.get_address())
         # print(model_ag.get_address())
 
     with tempfile.TemporaryDirectory() as tempdir:
-        # tempdir = design
+        tempdir = design
         shutil.copy(f"{frail_dir}/verilog/{design}.v", tempdir)
         tester.compile_and_run(target="verilator",
                                directory=tempdir,
                                skip_compile=True,
-                               flags=["-Wno-fatal"])
-                               # flags=["-Wno-fatal", "--trace"])
+                               #flags=["-Wno-fatal"])
+                               flags=["-Wno-fatal", "--trace"])
 
 
 if __name__ == "__main__":
-    test_addr_design(True, "op_design")#, 0, 15, 20, 12, 15)
+    test_addr_design(False, "og_design6")#, 0, 15, 20, 12, 15)
