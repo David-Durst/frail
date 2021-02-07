a3b648c032b6ad15865134299a763d776592f1eb
David Durst <davidbdurst@gmail.com>
2021-02-07 09:57:38 -0800

    fixing version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='frail',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/David-Durst/frail',
     license='MIT',
     maintainer='David Durst',
