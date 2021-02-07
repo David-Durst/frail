5061e85216ceaa248009dfcdd2547a0c60674bd7
David Durst <davidbdurst@gmail.com>
2021-02-07 10:01:07 -0800

    renaemd and commented lake designs

--- frail/examples.py
+++ frail/examples.py
@@ -2,6 +2,13 @@ from frail.ast import *
 from frail.debug_printer import *
 from frail.verilog_printer import *
 
+"""
+#lake paper source for below design
+def affine_addr(stride_0, range_0, stride_1, range_1, offset):
+    for i1 in range(range_1):
+        for i0 in range(range_0):
+            addr = i0 * stride_0 + i1 * stride_1 + offset
+"""
 
 # configuration registers
 x_max = var_f("x_max")
@@ -11,13 +18,39 @@ y_stride = var_f("y_stride")
 offset = var_f("offset")
 
 # normal affine
-def create_for_design():
+def create_affine_addr_design():
     x_unit_counter = scan_const_f(lambda z: if_f(eq_f(z, sub_f(x_max, int_f(1))), int_f(0), add_f(z, int_f(1))))
     y_unit_counter = scan_const_f(lambda z: if_f(eq_f(x_unit_counter.get_seq(), sub_f(x_max, int_f(1))), add_f(z, int_f(1)), z))
     design = scan_const_f(lambda z: add_f(add_f(mul_f(x_unit_counter.get_seq(), x_stride), mul_f(y_unit_counter.get_seq(), y_stride)), offset))
     return design
     
-for_design = create_for_design()
+affine_addr_design = create_affine_addr_design()
+
+
+"""
+#lake paper source for below design
+def piecewise_addr(stride_0_0, stride_0_1,
+                    range_0, 
+                    stride_1_0, stride_1_1, 
+                    range_1, 
+                    offset_0, offset_1, offset_2, offset_3,
+                    i0_piece,
+                    i1_piece):
+    for i1 in range(range_1):
+        for i0 in range(range_0):
+            if i1 < i1_piece and i0 < i0_piece:
+                addr = i0 * stride_0_0 + i1 * stride_1_0 
+                       + offset_0
+            elif i1 < i1_piece and i0 > i0_piece:
+                addr = i0 * stride_0_1 + i1 * stride_1_0 
+                       + offset_1
+            elif i1 > i1_piece and i0 < i0_piece:
+                addr = i0 * stride_0_0 + i1 * stride_1_1 
+                       + offset 2
+            else:
+                addr = i0 * stride_0_1 + i1 * stride_1_1 
+                       + offset 3
+"""
 
 x_max = var_f("x_max")
 y_max = var_f("y_max")
@@ -27,7 +60,7 @@ offset_0, offset_1, offset_2, offset_3 = var_f("offset_0"), var_f("offset_1"), v
 i0_piece, i1_piece = var_f("i0_piece"), var_f("i1_piece")
 
 # piecewise affine
-def create_piece_design():
+def create_piecewise_addr_design():
     x_unit_counter = scan_const_f(lambda z: if_f(eq_f(z, sub_f(x_max, int_f(1))), int_f(0), add_f(z, int_f(1))))
     y_unit_counter = scan_const_f(lambda z: if_f(eq_f(x_unit_counter.get_seq(), sub_f(x_max, int_f(1))), add_f(z, int_f(1)), z))
     design = scan_const_f(lambda z: 
@@ -43,7 +76,9 @@ def create_piece_design():
     )
     return design
     
-piece_design = create_piece_design()
+piecewise_addr_design = create_piecewise_addr_design()
+
+# END OF LAKE DESIGNS
 
 # original addressor design
 def create_og_design():
