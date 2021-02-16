90a7a59bb35d9f974ef82e065e924f1aeaecaa52
Kavya Sreedhar <skavya@stanford.edu>
2021-02-16 14:02:46 -0800

    default width 16

--- frail/examples.py
+++ frail/examples.py
@@ -84,7 +84,7 @@ def create_counter_design():
     level1 = counter_f(None, x_max, 1)
     #level2 = counter_f(level1.at_max(), y_max, 1)
     #add_design = scan_const_f(lambda z: add_f(offset, add_f(mul_f(level1.val(), x_stride), mul_f(level2.val(), y_stride))))
-    return scan_const_f(lambda z: level1.val()) #add_design
+    return level1.val() #add_design
 
 counter_design = create_counter_design()
 
