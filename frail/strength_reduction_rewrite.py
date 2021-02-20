5c5d46c671da74b09881163482abed0054aab24c
Kavya Sreedhar <skavya@stanford.edu>
2021-02-20 11:35:34 -0800

    more cleanup

--- frail/strength_reduction_rewrite.py
+++ frail/strength_reduction_rewrite.py
@@ -1,57 +1,12 @@
 from frail.ast import *
 from typing import Set, Dict, List
 
-@dataclass(eq=True, frozen=True)
-class ModulePort:
-    name: str
-    width: int
-    requires_clock: bool
-    input_dir: bool
-    input_from_other_scan: bool
-    # true if this is the max signal from a counter, false it's the val, None if this is not from a counter
-    counter_max: bool = None
 
-tab_str = "    "
-def get_tab_strs(num):
-    final_tab_str = ""
-    for i in range(num):
-        final_tab_str += tab_str
-    return final_tab_str
-
-recurrence_seq_str = "scan_output_"
-counter_max_output = "counter_at_max_"
-counter_val_output = "counter_val_"
-step_if_begin = get_tab_strs(2) + "if (step) begin\n"
-step_if_end = get_tab_strs(2) + "end \n"
-
-io_ports: Dict[int, List[ModulePort]] = {}
-io_strs: Dict[int, str] = {}
-# variables (logic type) used in the combinational block
-var_strs: Dict[int, str] = {}
-# combinational block
-comb_strs: Dict[int, str] = {}
-# sequential block (where the scan state is updated each clock)
-seq_strs: Dict[int, str] = {}
-printed_ops: Set[int] = set()
 cur_scan_idx: int = -1
 cur_scan_lambda_var: Var = None
 output_scan_index: int = -1
-VarTable: Dict[str, str] = {}
-config_regs: List[AST] = []
+printed_ops: Set[int] = set()
 mul_list = {}
-
-def verilog_header(index: int, name: str="scan"):
-    if name == "scan":
-        return f"module {name}{index} ("
-    else:
-        return f"module {name} ("
-verilog_footer = "endmodule\n"
-
-def get_var_val(key):
-    if key in VarTable.keys():
-        return VarTable[key]
-    return key
-
 
 def strength_reduction_rewrite(e: AST,
                                root: bool = True,
@@ -59,60 +14,22 @@ def strength_reduction_rewrite(e: AST,
                                top_name: str = "top",
                                add_step: bool = True,
                                get_verilog: bool = True):
-    global io_ports, io_strs, var_strs, comb_strs, seq_strs, printed_ops
-    global cur_scan_idx, output_scan_index, cur_scan_lambda_var, VarTable, config_regs
-    global mul_list
+    global cur_scan_idx, output_scan_index, cur_scan_lambda_var
+    global printed_ops, mul_list
 
     if root:
-        io_ports = {}
-        io_strs = {}
-        var_strs ={}
-        comb_strs = {}
-        seq_strs = {}
-        printed_ops = set()
         cur_scan_idx = -1
         output_scan_index = -1
-        config_regs = []
+        printed_ops = set()
+        mul_list = {}
 
     e_type = type(e)
     # print("INFO", cur_scan_idx, e_type)
 
     if e.index in printed_ops:
-        # still need to add op to io_ports for this next module
-        # for signals that are inputs to multiple submodules
-        # so that this signal is printed in Verilog
-        add_port = None
-        if e_type == Var:
-            if e == cur_scan_lambda_var:
-                add_port = ModulePort(e.name, e.width, True, False, False)
-            else:
-                add_port = ModulePort(e.name, e.width, False, True, False)
-        elif e_type == RecurrenceSeq:
-            width = get_width(e.index, lake_state)
-            add_port = ModulePort(recurrence_seq_str + str(e.producing_recurrence), width, False, True, True)
-
-        if add_port is not None and add_port not in io_ports[cur_scan_idx]:
-            io_ports[cur_scan_idx].append(add_port)
-            
         return
 
     printed_ops.add(e.index)
-
-    # start with empty string if printing expression not in a scan
-    if not io_ports:
-        io_ports[-1] = []
-
-    if not io_strs:
-        io_strs[-1] = ""
-
-    if not var_strs:
-        var_strs[-1] = ""
-
-    if not comb_strs:
-        comb_strs[-1] = ""
-
-    if not seq_strs:
-        seq_strs[-1] = ""
 
     replace_counter = None
     if e_type == RecurrenceSeq:
